9c38e3f65b9bed2095559f174cfd47cc781ef354
innsmouth_dweller <innsmouth.dweller@gmail.com>
2019-12-05 18:12:29 +0100

    Updates

---
+++ precache-manifest.bca0b992c5230f519ce1b99a4059f5b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7578229b09c658950ba5408dfcdfd07c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53b2f027a704a6c7222",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c306a9a291a76327699",
+    "url": "/static/js/2.295d81d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.295d81d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d53b2f027a704a6c7222",
+    "url": "/static/js/main.22a7386e.chunk.js"
+  },
+  {
+    "revision": "6a656f856e7a60c8aaea",
+    "url": "/static/js/runtime-main.84f61fc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
